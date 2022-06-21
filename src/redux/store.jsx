837e496cb324296b9908fb1b990c6a5f1f606763
Denis Abashin <79473861+fordelord@users.noreply.github.com>
2022-06-21 11:48:26 +0300

    test

--- src/redux/store.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { configureStore } from '@reduxjs/toolkit'
-import counterReducer from './slices/slice'
-
-export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
-})
